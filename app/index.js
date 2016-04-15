60c20240b93daa06c775bdada20cbcf3a23d2f85
Heitor Estrela Gomes <Heitor Estrela Gomes>
2016-04-15 18:48:38 -0300

    Adicionando bootstrap e ui-bootstrap e seus respectivos loaders

--- app/index.js
+++ app/index.js
@@ -4,10 +4,12 @@ import angular from 'angular';
 import oclazyload from 'oclazyload';
 import uirouter from 'angular-ui-router';
 import routes from './routes';
+import uibootstrap from 'angular-ui-bootstrap';
 
 var angularModule = angular.module('heitorpack', [
   uirouter,
-  'oc.lazyLoad'
+  'oc.lazyLoad',
+  uibootstrap
 ]);
 
 routes(angular, angularModule);
--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ var webpack = require('webpack');
 var config = {
   entry: {
     app: './app/index.js',
-    vendor: ['angular', 'angular-ui-router', 'oclazyload']
+    vendor: ['angular', 'angular-ui-router', 'oclazyload', 'bootstrap-webpack!./config/bootstrap.config.js', 'angular-ui-bootstrap']
   },
   output: {
     path: './build',
@@ -12,14 +12,31 @@ var config = {
   },
   module: {
     loaders: [{
-      test: /\.html$/,
-      loader: 'raw',
-      exclude: /node_modules/
-    }, {
-      test: /\.js$/,
-      loader: 'ng-annotate!babel',
-      exclude: /node_modules/
-    }]
+        test: /\.html$/,
+        loader: 'raw',
+        exclude: /node_modules/
+      }, {
+        test: /\.js$/,
+        loader: 'ng-annotate!babel',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url?limit=10000&mimetype=application/font-woff&name=./fonts/[name].[ext]'
+      },
+      {
+        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url?limit=10000&mimetype=application/octet-stream&name=./fonts/[name].[ext]'
+      },
+      {
+        test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'file?name=./fonts/[name].[ext]'
+      },
+      {
+        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url?limit=10000&mimetype=image/svg+xml&name=./fonts/[name].[ext]'
+      }
+    ]
   },
   plugins: [
     new webpack.optimize.CommonsChunkPlugin({
